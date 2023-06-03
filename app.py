67d1890b8a150219964e2a7307ef47b965d7a976
bemarinos <135247962+bemarinos@users.noreply.github.com>
2023-06-03 12:37:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ global cog_region
 
 try:
     cog_key = os.environ.get("COG_SERVICE_KEY")
-    cog_region = os.environ.get("COG_SERVICE_REGION")      
-    #translator_endpoint = 'https://api.cognitive.microsofttranslator.com'   
-    translator_endpoint='https://traductorbeatrizcognitive.cognitiveservices.azure.com/'
+    cog_region = os.environ.get("COG_SERVICE_REGION")  
+    #cog_key = "a28f8e30d3d5461190103ed5d68226d9"
+    #cog_region = "eastus"  
+    translator_endpoint = 'https://api.cognitive.microsofttranslator.com'   
+    #translator_endpoint='https://traductorbeatrizcognitive.cognitiveservices.azure.com/'
 except Exception as ex:        
     print(ex)
 
@@ -22,7 +24,7 @@ def GetLanguage(text):
     # # Use the Translator detect function
     path = '/detect'
     url = translator_endpoint + path
-
+    print(cog_key)
     # # Build the request
     params = {
          'api-version': '3.0'
@@ -49,15 +51,48 @@ def GetLanguage(text):
     # # Return the language
     return language
 
+def Translate(text, source_language):    
+    translation = ''    
+    # Use the Translator translate function    
+    path = '/translate'    
+    url = translator_endpoint + path    
+    
+    # Build the request    
+    params = {        
+              'api-version': '3.0',        
+              'from': source_language,        
+              'to': ['en']    
+    }    
+    
+    headers = {        
+               'Ocp-Apim-Subscription-Key': cog_key,        
+               'Ocp-Apim-Subscription-Region': cog_region, 
+               'Content-type': 'application/json'    
+    }    
+    body = [{        
+             'text': text    
+    }]    
+    
+    # Send the request and get response    
+    request = requests.post(url, params=params, headers=headers, json=body)    
+    response = request.json()    
+    
+    # Parse JSON array and get translation    
+    translation = response[0]["translations"][0]["text"]    
+    
+    # Return the translation    
+    return translation
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         text = request.form['text']
         print(text)
+        
         # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
         source_language = GetLanguage(text)
-        translated_text = text
-
+        translated_text =  Translate(text, source_language)
+        print(text)
         return render_template('home.html', translated_text=translated_text,lang_detected=source_language)
     
     return render_template('home.html')
